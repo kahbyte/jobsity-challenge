cdf2fabab68f1259051966a9334ccbee5e101cb6
kahbyte <kahbyte@gmail.com>
2024-03-03 18:03:58 -0300

    Initial Project

---
+++ Jobsity-Challenge/Jobsity_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jobsity_ChallengeApp.swift
+//  Jobsity-Challenge
+//
+//  Created by Kauê Sales on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Jobsity_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
