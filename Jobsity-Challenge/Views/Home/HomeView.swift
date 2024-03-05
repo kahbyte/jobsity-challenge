092b19892f219c86a28bd3e615cdcf75adc620de
kahbyte <kahbyte@gmail.com>
2024-03-05 00:40:14 -0300

    finished authentication

--- Jobsity-Challenge/Views/Home/HomeView.swift
+++ Jobsity-Challenge/Views/Home/HomeView.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 import Combine
+import LocalAuthentication
 
 struct HomeView: View {
     @StateObject var viewModel: HomeViewModel = .init()
     @State private var title: String = "Faz a request vain"
     @State private var page: Int = 1
     @State private var searchText: String = ""
+    @State var isUnlocked: Bool = false
     
     var columns: [GridItem] = [
         GridItem(.adaptive(minimum: 100))
@@ -21,45 +23,74 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             VStack {
-                SearchBar(text: $searchText) {
-                    viewModel.showLookup(searchText)
-                }
-                
-                ScrollView {
-                    if searchText.isEmpty {
-                        LazyVGrid(columns: columns, spacing: 16) {
-                            ForEach(viewModel.list) { show in
-                                NavigationLink(destination: ShowDetailsView(show: show)) {
-                                    TVShowVerticalView(showDetails: show)
-                                        .frame(width: 100)
-                                        .foregroundColor(.primary)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .onAppear {
-                                            if viewModel.list.last == show {
-                                                page += 1
-                                                viewModel.requestShowList(page: page)
+                if isUnlocked {
+                    SearchBar(text: $searchText) {
+                        viewModel.showLookup(searchText)
+                    }
+                    
+                    ScrollView {
+                        if searchText.isEmpty {
+                            LazyVGrid(columns: columns, spacing: 16) {
+                                ForEach(viewModel.list) { show in
+                                    NavigationLink(destination: ShowDetailsView(show: show)) {
+                                        TVShowVerticalView(showDetails: show)
+                                            .frame(width: 100)
+                                            .foregroundColor(.primary)
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                            .onAppear {
+                                                if viewModel.list.last == show {
+                                                    page += 1
+                                                    viewModel.requestShowList(page: page)
+                                                }
                                             }
                                     }
                                 }
                             }
-                        }
-                    } else {
-                        LazyVStack(alignment: .leading) {
-                            ForEach(viewModel.searchList) { result in
-                                NavigationLink(destination: ShowDetailsView(show: result)) {
-                                    TVShowHorizontalView(showDetails: result)
-                                        .frame(height: 100)
-                                        .foregroundColor(.primary)
+                        } else {
+                            LazyVStack(alignment: .leading) {
+                                ForEach(viewModel.searchList) { result in
+                                    NavigationLink(destination: ShowDetailsView(show: result)) {
+                                        TVShowHorizontalView(showDetails: result)
+                                            .frame(height: 100)
+                                            .foregroundColor(.primary)
+                                    }
                                 }
                             }
                         }
                     }
+                    .padding()
+                    .onAppear {
+                        viewModel.requestShowList(page: 1)
+                    }
                 }
-                .padding()
-                .onAppear {
-                    viewModel.requestShowList(page: 1)
+                else {
+                    Text("Não autenticado")
                 }
             }
+            .onAppear {
+                authenticate()
+            }
+        }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Nós precisamos autenticar você para desbloquear o aplicativo."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        self.isUnlocked = false
+                    }
+                }
+            }
+        } else {
+            self.isUnlocked = false
         }
     }
 }
